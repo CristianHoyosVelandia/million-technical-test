a8c4a4e9e27f2a01d9f8960fa977104e279a6e75
CristianHoyosVelandia <cristian-hoyos@hotmail.com>
2025-11-18 17:21:26 -0500

    FEATURE: "Error pagina 404"

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import styles from './NotFound.module.css';
-
 const NotFound = () => {
   const navigate = useNavigate();
-
   return (
     <div className="container">
       <div className={styles.notFound}>
@@ -13,8 +11,7 @@ const NotFound = () => {
           La página que buscas no existe o fue movida.
         </p>
         <button onClick={() => navigate('/')} className={styles.button}>
-          <i className="bi bi-house-door"></i>
-          Ir al inicio
+          <i className="bi bi-house-door"></i> Ir al inicio
         </button>
       </div>
     </div>
